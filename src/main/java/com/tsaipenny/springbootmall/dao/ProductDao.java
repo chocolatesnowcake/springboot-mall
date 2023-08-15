ddc3ed840ff5a68e5c5f8e094e90ce88d7d3b9b7
Pei Hua Tsai <108539553+chocolatesnowcake@users.noreply.github.com>
2023-08-16 02:53:17 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/tsaipenny/springbootmall/dao/ProductDao.java
+++ src/main/java/com/tsaipenny/springbootmall/dao/ProductDao.java
@@ -3,9 +3,11 @@ package com.tsaipenny.springbootmall.dao;
 import com.tsaipenny.springbootmall.dto.ProductRequest;
 import com.tsaipenny.springbootmall.model.Product;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/tsaipenny/springbootmall/service/ProductService.java
+++ src/main/java/com/tsaipenny/springbootmall/service/ProductService.java
@@ -3,7 +3,10 @@ package com.tsaipenny.springbootmall.service;
 import com.tsaipenny.springbootmall.dto.ProductRequest;
 import com.tsaipenny.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
